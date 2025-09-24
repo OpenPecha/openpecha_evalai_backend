ec0b896f30b3eb0798809baee59dd4bd8ddf1fb2
Tenzin  Tsering <tenzintsering630@gmail.com>
2025-09-24 09:23:27 +0530

    fix TextCategory id field

--- models/text_category.py
+++ models/text_category.py
@@ -9,7 +9,7 @@ class TextCategory(Base):
 
     __tablename__ = "text_category"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4()), unique=True, nullable=False)
     name = Column(String, nullable=False)
     created_at = Column(String, nullable=False, default=lambda: datetime.datetime.now(datetime.timezone.utc))
     updated_at = Column(String, nullable=False, default=lambda: datetime.datetime.now(datetime.timezone.utc), onupdate=lambda: datetime.datetime.now(datetime.timezone.utc))
